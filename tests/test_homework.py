69a37e154d40b30ac1058cf6b033012570326542
danlondonova <danlondonova@unal.edu.co>
2025-05-05 20:02:46 -0500

    resultado

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -8,29 +8,29 @@ def test_homework():
     """Test Word Count"""
 
     for path in [
-        "homework/src",
-        "homework/src/_internals",
-        "homework/src/_internals/count_words.py",
-        "homework/src/_internals/preprocess_lines.py",
-        "homework/src/_internals/read_all_lines.py",
-        "homework/src/_internals/split_into_words.py",
-        "homework/src/_internals/write_word_counts.py",
+        "homework\src",
+        "homework\src\_internals",
+        "homework\src\_internals\count_words.py",
+        "homework\src\_internals\preprocess_lines.py",
+        "homework\src\_internals\read_all_lines.py",
+        "homework\src\_internals\split_into_words.py",
+        "homework\src\_internals\write_word_counts.py",
     ]:
         if not os.path.exists(path):
             raise Exception(f"'{path}' directory does not exist")
 
     try:
         subprocess.run(
-            ["python3", "-m", "homework", "data/input", "data/output"],
+            ["python3", "-m", "homework", "data\input", "data\output"],
             check=True,
         )
     except subprocess.CalledProcessError as e:
         raise Exception(f"Error running the homework script: {e}")
 
-    if not os.path.exists("data/output/"):
-        raise Exception("'data/output/' directory does not exist")
+    if not os.path.exists("data\output"):
+        raise Exception("'data\output\' directory does not exist")
 
-    results_file = "data/output/wordcount.tsv"
+    results_file = "data\output\wordcount.tsv"
     if not os.path.exists(results_file):
         raise Exception(f"'{results_file}' file does not exist")
 
